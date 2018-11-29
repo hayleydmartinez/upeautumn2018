041bc3e39b7ad11ac5dee2ae885cf73dce61fc12
Hayley Martinez <38091293+hayleydmartinez@users.noreply.github.com>
2018-11-28 20:37:14 -0800

    Working solution

--- escape.py
+++ escape.py
@@ -2,7 +2,14 @@
 import requests
 import json
 
-def escape(url, seen):
+actions = [ 'RIGHT', 'UP', 'LEFT', 'DOWN']
+opp     = { 'RIGHT':'LEFT', 'UP':'DOWN', 'LEFT':'RIGHT', 'DOWN':'UP' }
+mx, my  = 0, 0
+
+def valid(x, y):
+    return x > -1 and x < mx and y > -1 and y < my
+
+def escape(url, seen, stack):
     # REFRESH DATA
     r    = requests.get(url)
     data = json.loads(r.content)
@@ -10,84 +17,65 @@ def escape(url, seen):
     # LOCATION VARIABLES
     x = data['current_location'][0]
     y = data['current_location'][1]
-    seen[y][x] = '+'
+    seen.add((str(x), str(y)))
 
     # MOVEMENT VARIABLES
-    actions = [ 'UP', 'DOWN', 'RIGHT', 'LEFT']
-    opp     = { 'UP':'DOWN', 'DOWN':'UP', 'RIGHT':'LEFT', 'LEFT':'RIGHT'}
     moves   = { 'UP':[x,y-1], 'DOWN':[x,y+1], 'RIGHT':[x+1,y], 'LEFT':[x-1,y] }
 
     for i in actions:
-        print("We are at: [" + str(x) + ',' + str(y) + ']')
-        print('Try to move ' + i)
-        for j in seen:
-            print(j)
+        coord = (str(moves[i][0]), str(moves[i][1]))
 
-        # STEP IN DIRECTION
-        direction = requests.post(url, data = { 'action': i })
-        result = json.loads(direction.content)['result']
+        if valid(moves[i][0], moves[i][1]) and coord not in seen:
 
-        print(result)
+            # step in direction
+            direction = requests.post(url, data = { 'action': i })
+            result = json.loads(direction.content)['result']
 
-        if result == 'END':
-            return True
-
-        if result == 'OUT_OF_BOUNDS':
-            continue
-
-        # REFRESH DATA
-        r    = requests.get(url)
-        data = json.loads(r.content)
-
-        # LOCATION VARIABLES
-        x = data['current_location'][0]
-        y = data['current_location'][1]
-
-        if result == 'WALL':
-            seen[moves[i][1]][moves[i][0]] = 'X'
-            continue
-
-        # IF YOU HAVEN'T BEEN TO THIS SPOT
-        if seen[y][x] == ' ':
-            print('Move ' + i)
-            if escape(url, seen):
+            # if we are at the end, we have escaped
+            if result == 'END':
                 return True
 
-        # IF YOU'VE ALREADY BEEN, STEP BACK
-        elif seen[y][x] != 'X':
-            requests.post(url, data = { 'action': opp[i] })
-            r = requests.get(url)
-            data = json.loads(r.content)
+            # if we can't move there, we add to seen so we don't re-check it
+            elif result == 'OUT_OF_BOUNDS' or result == 'WALL':
+                seen.add(coord)
+                continue
 
+            # add our move to the stack
+            stack.append(i)
+            print(stack)
+
+            # if we have escaped, we're done!
+            if escape(url, seen, stack):
+                return True
+
+    # if we can't move anywhere, we undo our last action
+    requests.post(url, data = { 'action': opp[stack.pop()] })
     return False
 
 
-# POST TO SERVER
+
+# post to server
 r = requests.post('http://ec2-34-216-8-43.us-west-2.compute.amazonaws.com/session', data = { 'uid': '104926567' })
 token = json.loads(r.content)['token']
 url = 'http://ec2-34-216-8-43.us-west-2.compute.amazonaws.com/game?token=' + token
 
-# GET DATA FROM SERVER
+# get data from server
 r = requests.get(url)
 data = json.loads(r.content)
 
 while(data['levels_completed'] != data['total_levels']):
-    # ESCAPE FROM MAZE
+    #
     print ("Begin maze")
     print (data['maze_size'])
 
-    seen = []
-    for i in range(data['maze_size'][1]):
-        item = []
-        for j in range(data['maze_size'][0]):
-            item.append(' ')
-        seen.append(item)
+    mx, my = data['maze_size'][0], data['maze_size'][1]
+    seen = set()
+    stack = []
+    escape(url, seen, stack)
 
-    escape(url, seen)
-
-    # REFRESH DATA EVERY LEVEL
+    # refresh data
     r = requests.get(url)
     data = json.loads(r.content)
-    print("Escaped " + str(data['levels_completed']) + " mazes!")
+    print("Escaped the maze!")
 
 print("Finished all mazes!")
